a332b0e73ffabf98958b646c0a7fae1aec718264
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-03-02 12:34:19 +0300

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Write your code here!document.querySelector("main#main").remove();
+document.querySelector("main#main").remove();
+// Create a new <h1> element
+
+const newHeader = document.createElement("h1");
+
+// Set the id to "victory"
+newHeader.id = "victory";
+
+// Set the text content with your name
+newHeader.textContent = "Naima is the champion";
+
+// Append it to the body
+document.body.appendChild(newHeader);
